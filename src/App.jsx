7b7ba20ceb1f88a8513058989ec604fac65170ec
Juan Luis Garcia Garcia <juanluisggarcia@gmail.com>
2024-01-08 13:16:11 +0100

    finalizado bucles y key

--- src/App.jsx
+++ src/App.jsx
@@ -22,9 +22,10 @@ const App = () => {
 				<div className='wrapper'>
 					<div className='flex'>
 						{/* <div className='col'> */}
+							
 							{/* Rellenando componentes con bucles y key */}
 
-							{/* {cardInfo.map(card => (
+							{cardInfo.map(card => (
 								<Card
 									key={v4()}
 									color={card.color}
@@ -32,30 +33,21 @@ const App = () => {
 									text={card.text}
 									icon={card.icon}
 								/>
-							))} */}
+							))}
 
-							{/* {cardInfo.map(card => (
-								<TestCard
-									key={v4()}
-									color={card.color}
-									bgColor={card.bgColor}
-									path={card.path}
-									test={card.test}
-									punctuation={card.punctuation}
-								/>
-							))} */}
+							
 
-							<Card color="border__cyan" title="Supervisor" text="Monitors activity to identify project roadblocks" icon="./assets/images/icon-supervisor.svg" />
+							{/* <Card color="border__cyan" title="Supervisor" text="Monitors activity to identify project roadblocks" icon="./assets/images/icon-supervisor.svg" /> */}
 						{/* </div>
 
 						{/* <div className='col'> */}
-							<Card color="border__red" title="Team Builder" text="Scans our talent network to create the optimal team for your project" icon="./assets/images/icon-team-builder.svg" /> 
+							{/* <Card color="border__red" title="Team Builder" text="Scans our talent network to create the optimal team for your project" icon="./assets/images/icon-team-builder.svg" />  */}
 
-							<Card color="border__orange" title="Karma" text="Regularly evaluates our talent to ensure quality" icon="./assets/images/icon-karma.svg" />
+							{/* <Card color="border__orange" title="Karma" text="Regularly evaluates our talent to ensure quality" icon="./assets/images/icon-karma.svg" /> */}
 						{/* </div> */}
 
 						{/* <div className='col'> */}
-							<Card color="border__blue" title="Calculator" text="Uses data from past projects to provide better delivery estimates" icon="./assets/images/icon-calculator.svg" />
+							{/* <Card color="border__blue" title="Calculator" text="Uses data from past projects to provide better delivery estimates" icon="./assets/images/icon-calculator.svg" /> */}
 						{/* </div> */}
 					</div>
 				</div>
